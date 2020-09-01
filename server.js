22b2c756b9fb788b2ebf401480158e014e558fbc
Tyspice <tyner27@gmail.com>
2020-09-01 15:40:03 -0700

    added db password and name as env vars

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const app = require('./app');
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-const mongoDB = process.env.DB.replace('<password>', process.env.DB_PASS);
+const mongoDB = process.env.DB
+  .replace('<dbname>', process.env.DB_NAME)
+  .replace('<password>', process.env.DB_PASSWORD);
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
