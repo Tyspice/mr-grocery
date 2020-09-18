b9393be2c12b31dd77d7c70aaff6da4b0ce788d1
Tyspice <tyner27@gmail.com>
2020-09-18 13:37:00 -0700

    added build folder

---
+++ build/precache-manifest.54c8a05e6296a7007fa4678e614da4f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0551499bfbcc79ac37e1ea7b42a72841",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13df928f96333748b9b9",
+    "url": "/static/css/2.a1f2881c.chunk.css"
+  },
+  {
+    "revision": "13df928f96333748b9b9",
+    "url": "/static/js/2.ebde4252.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.ebde4252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da21dfb80807a89c4e0e",
+    "url": "/static/js/main.2aabae5e.chunk.js"
+  },
+  {
+    "revision": "3eb6db2a3baf27c5088d",
+    "url": "/static/js/runtime-main.4933886a.js"
+  }
+]);
