a0a9c8374fda330033caa95fc16abcb5fe7e30de
Tyspice <tyner27@gmail.com>
2020-09-28 12:20:03 -0700

    disabled sms feature for now

--- controllers/smsController.js
+++ controllers/smsController.js
@@ -1,11 +1,11 @@
-const requestHandler = require('../sms/requestHandler');
+// const requestHandler = require('../sms/requestHandler');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 exports.handleSmsRequest = async (req, res) => {
     const twiml = new MessagingResponse();
   
-    message = await requestHandler(req); 
-    twiml.message(message);
+    // message = await requestHandler(req); 
+    twiml.message('sms functionality currently disabled.\nplease visit https://mr-grocery.herokuapp.com to use this application');
   
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
